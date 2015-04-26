8314ccda594287b93c97f8577da793225d031ce9
Kamil Adryjanek <kamil.adryjanek@gmail.com>
2015-04-26 11:57:28 +0200

    Dodany loader dla  srodowiska test

--- src/AppBundle/Tests/Controller/ProductControllerTest.php
+++ src/AppBundle/Tests/Controller/ProductControllerTest.php
@@ -3,9 +3,37 @@
 namespace AppBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\Common\DataFixtures\Loader;
 
 class ProductControllerTest extends WebTestCase
 {
+    protected static $application;
+
+    protected function setUp()
+    {
+        static::$kernel = static::createKernel();
+		static::$kernel->boot();
+		$this->em = static::$kernel->getContainer()
+			->get('doctrine')
+			->getManager();
+        
+        // reset db
+		$purger = new ORMPurger($this->em);
+		$purger->purge();
+        
+        $loader = new Loader();
+		$loader->addFixture(new \AppBundle\DataFixtures\ORM\LoadUserData());
+        $loader->addFixture(new \AppBundle\DataFixtures\ORM\LoadCategoryData());
+        $loader->addFixture(new \AppBundle\DataFixtures\ORM\LoadProductsData());
+		
+		$purger = new ORMPurger($this->em);
+		$executor = new ORMExecutor($this->em, $purger);
+		$executor->execute($loader->getFixtures());
+    }
+
+    
     public function testAddToCart()
     {
         $client = static::createClient();
